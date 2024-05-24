e7dd076eb97a7c7f98e4310942f60e06fdbb2a14
Jacob Toftgaard <jacobjshelp@gmail.com>
2024-05-24 14:18:50 +0400

    Step 2 - The initial game loop and rendering with 30 fps

---
+++ gameLogic/gameConstants.ts
@@ -0,0 +1,4 @@
+const FPS = 30
+const SLEEP = 1000 / FPS
+
+export { SLEEP }
--- gameLogic/index.ts
+++ gameLogic/index.ts
@@ -1,1 +1,28 @@
-console.log('Hello from the game logic')
+import { SLEEP } from "./gameConstants"
+
+function createGraphics() {
+  let canvas = document.getElementById('GameCanvas') as HTMLCanvasElement
+  let g = canvas.getContext('2d')
+  if (g) {
+    return g
+  } else throw Error('Canvas was not found on the page')
+}
+
+function gameLoop(g: CanvasRenderingContext2D) {
+  let before = Date.now()
+  g.clearRect(0, 0, 600, 400)
+  const currentSecond = `${new Date().getSeconds() % 9}f`
+  const color = '#4433' + currentSecond
+  console.log(color);
+  g.fillStyle = color
+  g.fillRect(0, 0, 600, 400)
+  let after = Date.now()
+  let frameTime = after - before
+  let sleep = SLEEP - frameTime
+  setTimeout(() => { gameLoop(g) }, sleep);
+}
+
+window.onload = () => {
+  const g = createGraphics()
+  gameLoop(g)
+}
